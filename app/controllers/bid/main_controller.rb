e035c4ab3aa2768224a44545622464be74a87979
川端洋平 <bata44883@gmail.com>
2017-02-10 16:34:21 +0900

    パスワード変更フォーム

--- app/controllers/bid/main_controller.rb
+++ app/controllers/bid/main_controller.rb
@@ -1,10 +1,21 @@
 class Bid::MainController < Bid::ApplicationController
   skip_before_action :check_rule, only: [:rule, :rule_update]
   skip_before_action :authenticate_company!, only: [:rule, :rule_update]
+  
   def index
   end
 
   def edit_password
+    @company = current_company
+  end
+
+  def update_password
+    @company = current_company
+    if @company.update_with_password(password_params)
+      redirect_to "/bid/", notice: 'パスワードを変更しましたので、再度ログインしてください'
+    else
+      render :edit_password
+    end
   end
 
   def rule
@@ -13,5 +24,11 @@ class Bid::MainController < Bid::ApplicationController
   def rule_update
     session[:bid_rule] = Time.now
     redirect_to "/bid/login"
+  end
+
+  private
+
+  def password_params
+    params.require(:company).permit(:password, :password_confirmation, :current_password)
   end
 end
--- app/controllers/system/main_controller.rb
+++ app/controllers/system/main_controller.rb
@@ -3,5 +3,21 @@ class System::MainController < System::ApplicationController
   end
 
   def edit_password
+    @system = current_system
+  end
+
+  def update_password
+    @system = current_system
+    if @system.update_with_password(password_params)
+      redirect_to "/system/", notice: 'パスワードを変更しましたので、再度ログインしてください'
+    else
+      render :edit_password
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:system).permit(:password, :password_confirmation, :current_password)
   end
 end
