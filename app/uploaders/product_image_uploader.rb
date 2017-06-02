6f874a3329a0829216263a3b9d94945d03e57e2a
川端洋平 <bata44883@gmail.com>
2017-06-02 17:21:45 +0900

    アップロードファイル名

--- app/uploaders/product_image_uploader.rb
+++ app/uploaders/product_image_uploader.rb
@@ -38,4 +38,14 @@ class ProductImageUploader < CarrierWave::Uploader::Base
       img
     end
   end
+
+  def filename
+    "#{model.id}_#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+  def secure_token
+     var = :"@#{mounted_as}_secure_token"
+     model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  end
 end
