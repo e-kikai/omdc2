9c8af60c6c432cb30d721e8af44975183bec6b20
ba-ta <bata44883@gmail.com>
2020-12-24 11:15:31 +0900

    stagingメール送信先変更_02

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -13,8 +13,8 @@ ActionMailer::Base.smtp_settings = {
 }
 
 case Rails.env
-# when "staging"
-#   ActionMailer::Base.default_url_options = { host: '52.198.41.71' }
+when "staging"
+  ActionMailer::Base.default_url_options = { host: '52.198.41.71' }
 #   ActionMailer::Base.register_interceptor(MailInterceptor) # 自分宛てに転送
 when "development"
   ActionMailer::Base.default_url_options = { host: '192.168.33.110', port: 8082 }
