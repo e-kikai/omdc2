17d64b5a506aad5f62b69b419f8348d1bb344deb
ba-ta <bata44883@gmail.com>
2022-02-18 02:59:33 +0900

    MAI_テストメール_8

--- app/mailers/recommend_mailer.rb
+++ app/mailers/recommend_mailer.rb
@@ -11,7 +11,7 @@ class RecommendMailer < ApplicationMailer
     mail(
       to:       @mail,
       reply_to: "nyusatsu@omdc.or.jp",
-      subject:  "大阪機械卸業団地協同組合 #{@open.name} おすすめ出品商品"
+      subject:  "大阪機械卸業団地協同組合 #{@open.name} あなたへのおすすめ出品商品"
     )
   end
 end
