2b69382874da5dba57081088d03cc8d011979738
ba-ta <bata44883@gmail.com>
2023-03-24 18:03:02 +0900

    集計ページ_09

--- app/controllers/system/total_controller.rb
+++ app/controllers/system/total_controller.rb
@@ -148,8 +148,8 @@ class System::TotalController < System::ApplicationController
       products  = Product.group(:open_id)
       details   = products.joins(:detail_logs)
       favorites = products.joins(:favorites)
-      deletes   = favorites.merge(Favorite.unscope).where("favorites.soft_destroyed_at IS NOT NULL")
-      pdfs      = favorites.merge(Favorite.unscope).where("favorites.amount IS NOT NULL")
+      deletes   = favorites.merge(Favorite.unscoped).where("favorites.soft_destroyed_at IS NOT NULL")
+      pdfs      = favorites.merge(Favorite.unscoped).where("favorites.amount IS NOT NULL")
 
       @sql = favorites.to_sql
 
