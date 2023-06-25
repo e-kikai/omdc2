7efb3f3dfb8907548169bcf78e3e0d673df4dc40
ba-ta <bata44883@gmail.com>
2023-06-25 11:27:03 +0900

    各種集計全体_04

--- app/controllers/system/total_controller.rb
+++ app/controllers/system/total_controller.rb
@@ -2,14 +2,13 @@ class System::TotalController < System::ApplicationController
   include Exports
 
   def index
-    @open_selector         = Open.order(bid_end_at: :desc).pluck(:name, :id)
-    @total_selector        = {
+    @open_selector  = Open.order(bid_end_at: :desc).pluck(:name, :id)
+    @total_selector = {
         "検索方法/詳細アクセス、一山"   => :search_hitoyama_by_period,
         "リンク元/詳細アクセス、一山"   => :links_hitoyama_by_period,
         "出品会社/デメ半手数料"      => :company_deme,
         "価格帯/落札結果金額"       => :price_amount,
         "日別/アクセス,お気に入り利用"  => :date_favorite,
-      # "入札会/アクセス,お気に入り利用" => :opens_favorite,
         "エリア/出品・落札結果金額"    => :area_amount,
         "目玉商品/結果一覧"        => :feature_products,
     }
@@ -34,87 +33,87 @@ class System::TotalController < System::ApplicationController
     end
   end
 
-  def goal
-    sql = %q{
-    SELECT
-    o.id AS ID,
-    o.name AS "入札会名",
-    tp.s_min_price AS "出品最低入札価格(円)",
-    tp.c_id AS "出品数",
-    tb.s_amount AS "落札金額(円)",
-    tp.c_success_bid_id AS "落札数",
-    tb.c_id AS "入札数",
-    ROUND(tp.c_success_bid_id * 100.0 / tp.c_id, 2) AS "落札割合(%)",
-    tp.c_hitoyama AS "一山出品数",
-    ROUND(tp.c_hitoyama * 100.0 / tp.c_id, 2) AS "一山出品割合(%)",
-    tdl.c_utag AS "utag数",
-    tdl.c_id AS "詳細閲覧",
-    tdl.c_hitoyama AS "一山閲覧",
-    ROUND(tdl.c_hitoyama * 100.0 / tdl.c_id, 2) AS "一山閲覧割合(%)"
-  FROM
-    opens o
-  LEFT JOIN (
-      SELECT
-        p.open_id,
-        count(DISTINCT p.id) AS c_id,
-        sum(p.min_price) AS s_min_price,
-        count(DISTINCT p.success_bid_id) AS c_success_bid_id,
-        sum(CASE WHEN p.hitoyama = 'true' OR p.name ~ '(一山|1山|雑品)' THEN 1 END) AS c_hitoyama
-      FROM
-        products p
-      WHERE
-        p.soft_destroyed_at IS NULL
-      GROUP BY
-        p.open_id
-    ) tp ON
-    tp.open_id = o.id
-  LEFT JOIN (
-      SELECT
-        p2.open_id,
-        count(b.id) AS c_id,
-        sum(CASE WHEN b.id = p2.success_bid_id THEN b.amount END) AS s_amount
-      FROM
-        products p2
-      LEFT JOIN bids b ON
-        b.product_id = p2.id
-      WHERE
-        p2.soft_destroyed_at IS NULL
-        AND b.soft_destroyed_at IS NULL
-      GROUP BY
-        p2.open_id
-    ) tb ON tb.open_id = o.id
-    LEFT JOIN (
-      SELECT
-        p3.open_id,
-        count(dl.id) AS c_id,
-        count(DISTINCT dl.utag) as c_utag,
-        count(CASE WHEN p3.hitoyama = 'true' OR p3.name ~ '(一山|1山|雑品)'  AND dl.id IS NOT NULL THEN 1 END) AS c_hitoyama
-      FROM
-        detail_logs dl
-      LEFT JOIN products p3 ON
-        dl.product_id = p3.id
-      WHERE
-        p3.soft_destroyed_at IS NULL
-      GROUP BY
-        p3.open_id
-    ) tdl ON tdl.open_id = o.id
-  WHERE
-    o.soft_destroyed_at IS NULL
-  ORDER BY
-    o.id
-}
+#   def goal
+#     sql = %q{
+#     SELECT
+#     o.id AS ID,
+#     o.name AS "入札会名",
+#     tp.s_min_price AS "出品最低入札価格(円)",
+#     tp.c_id AS "出品数",
+#     tb.s_amount AS "落札金額(円)",
+#     tp.c_success_bid_id AS "落札数",
+#     tb.c_id AS "入札数",
+#     ROUND(tp.c_success_bid_id * 100.0 / tp.c_id, 2) AS "落札割合(%)",
+#     tp.c_hitoyama AS "一山出品数",
+#     ROUND(tp.c_hitoyama * 100.0 / tp.c_id, 2) AS "一山出品割合(%)",
+#     tdl.c_utag AS "utag数",
+#     tdl.c_id AS "詳細閲覧",
+#     tdl.c_hitoyama AS "一山閲覧",
+#     ROUND(tdl.c_hitoyama * 100.0 / tdl.c_id, 2) AS "一山閲覧割合(%)"
+#   FROM
+#     opens o
+#   LEFT JOIN (
+#       SELECT
+#         p.open_id,
+#         count(DISTINCT p.id) AS c_id,
+#         sum(p.min_price) AS s_min_price,
+#         count(DISTINCT p.success_bid_id) AS c_success_bid_id,
+#         sum(CASE WHEN p.hitoyama = 'true' OR p.name ~ '(一山|1山|雑品)' THEN 1 END) AS c_hitoyama
+#       FROM
+#         products p
+#       WHERE
+#         p.soft_destroyed_at IS NULL
+#       GROUP BY
+#         p.open_id
+#     ) tp ON
+#     tp.open_id = o.id
+#   LEFT JOIN (
+#       SELECT
+#         p2.open_id,
+#         count(b.id) AS c_id,
+#         sum(CASE WHEN b.id = p2.success_bid_id THEN b.amount END) AS s_amount
+#       FROM
+#         products p2
+#       LEFT JOIN bids b ON
+#         b.product_id = p2.id
+#       WHERE
+#         p2.soft_destroyed_at IS NULL
+#         AND b.soft_destroyed_at IS NULL
+#       GROUP BY
+#         p2.open_id
+#     ) tb ON tb.open_id = o.id
+#     LEFT JOIN (
+#       SELECT
+#         p3.open_id,
+#         count(dl.id) AS c_id,
+#         count(DISTINCT dl.utag) as c_utag,
+#         count(CASE WHEN p3.hitoyama = 'true' OR p3.name ~ '(一山|1山|雑品)'  AND dl.id IS NOT NULL THEN 1 END) AS c_hitoyama
+#       FROM
+#         detail_logs dl
+#       LEFT JOIN products p3 ON
+#         dl.product_id = p3.id
+#       WHERE
+#         p3.soft_destroyed_at IS NULL
+#       GROUP BY
+#         p3.open_id
+#     ) tdl ON tdl.open_id = o.id
+#   WHERE
+#     o.soft_destroyed_at IS NULL
+#   ORDER BY
+#     o.id
+# }
 
-    @result = ActiveRecord::Base.connection.select_all(sql)
+#     @result = ActiveRecord::Base.connection.select_all(sql)
 
-    respond_to do |format|
-      format.html
-      format.csv { export_csv "goal.csv", :index }
-    end
-  end
+#     respond_to do |format|
+#       format.html
+#       format.csv { export_csv "goal.csv", :index }
+#     end
+#   end
 
   def opens
     @total_selector = {
-      "指標目標"            => :target,
+      "指標目標"            => :goal,
       "アクセス,お気に入り" => :favorites,
       "目玉商品"            => :features,
     }
@@ -127,13 +126,10 @@ class System::TotalController < System::ApplicationController
     end
 
     # 入札会一覧
-    opens_base = Open.where("opens.id >= ?", start_open_id)
-    @opens     = opens_base.order(bid_end_at: :asc).pluck(:id, :name)
-
-    @title = "入札会別 - #{@total_selector.key(@total)}"
+    @opens = Open.where("opens.id >= ?", start_open_id).order(:id).pluck(:id, :name)
+    @title = @total_selector.key(@total)
 
     products = Product.group(:open_id)
-    hitoyama = products.where("products.hitoyama = 'true' OR products.name ~ '(一山|1山|雑品)'")
 
     @results = case @total
       when :features
@@ -181,6 +177,11 @@ class System::TotalController < System::ApplicationController
         }
       else
         {
+          hitoyama = products.where("products.hitoyama = 'true' OR products.name ~ '(一山|1山|雑品)'")
+
+          product_details = products.joins(:detail_logs).count("detail_logs.id")
+          hitoyama_details = hitoyama.joins(:detail_logs).count("detail_logs.id")
+
           "出品数"    => products.count,
           "出品最低入札価格(円)"   => products.sum(:min_price),
           "出品会社数"  => products.distinct.count(:company_id),
@@ -191,12 +192,10 @@ class System::TotalController < System::ApplicationController
           "一山出品数"    => hitoyama.count,
           "一山出品率(%)" => percents(products.count, hitoyama.count),
           "utag数" => products.joins(:detail_logs).distinct.count("detail_logs.utag"),
-          "詳細閲覧" => products.joins(:detail_logs).count("detail_logs.id"),
-          "一山閲覧" => hitoyama.joins(:detail_logs).count("detail_logs.id"),
-          "一山閲覧率(%)" => percents(products.joins(:detail_logs).count("detail_logs.id"), hitoyama.joins(:detail_logs).count("detail_logs.id")),
-
+          "詳細閲覧" => product_details,
+          "一山閲覧" => hitoyama_details,
+          "一山閲覧率(%)" => percents(product_details, hitoyama_details),
         }
-
       end
 
     respond_to do |format|
@@ -553,167 +552,6 @@ LEFT JOIN
 ORDER BY
   d.date;
 }
-      #     when :opens_favorite
-      #     %q{
-      # SELECT
-      #   o.id,
-      #   regexp_replace(o.name, '第([0-9]+)回(.*)', '第\1回') as "入札会",
-      #   tb6.user_c AS "ユーザ(累計)",
-      #   COALESCE(tb6.user_c - LAG(tb6.user_c, 1) OVER (
-      #   ORDER BY
-      #     o.id
-      #   ), tb6.user_c) AS "ユーザ(新規)",
-      #   tb8.pc AS "出品数",
-      #   tb7.detail_c AS "詳細(件)",
-      #   tb7.detail_utag AS "詳細(utag)",
-      #   tb7.detail_u AS "詳細(ログイン人)",
-      #   tb7.detail_p AS "詳細(商品数)",
-      #   tb1.fa_c AS "お気に入り(件)",
-      #   tb1.fa_u AS "お気に入り(人)",
-      #   tb1.fa_p AS "お気に入り(商品)",
-      #   tb3.delete_c AS "削除(件)",
-      #   tb3.delete_u AS "削除(人)",
-      #   tb2.pdf_c AS "PDF生成(件)",
-      #   tb2.pdf_u AS "PDF生成(人)",
-      #   tb2.pdf_p AS "PDF生成(商品)"
-      #   -- tb4.pdf_delete_c AS "PDF&削除(件)",
-      #   -- tb4.pdf_delete_u AS "PDF&削除(人)"
-      #   -- tb5.pdf_miss_c AS "短時間で削除(件)",
-      #   -- tb5.pdf_miss_u AS "短時間で削除(人)"
-      # FROM
-      #   opens o
-      # LEFT JOIN (
-      #     SELECT
-      #       p2.open_id,
-      #       count(f2.id) AS fa_c,
-      #       count(DISTINCT f2.user_id) AS fa_u,
-      #       count(DISTINCT f2.product_id) AS fa_p
-      #     FROM
-      #       favorites f2
-      #     LEFT JOIN products p2 ON
-      #       p2.id = f2.product_id
-      #     WHERE
-      #       p2.soft_destroyed_at IS NULL
-      #     GROUP BY
-      #       p2.open_id
-      #   ) tb1 ON
-      #   tb1.open_id = o.id
-      # LEFT JOIN (
-      #     SELECT
-      #       p2.open_id,
-      #       count(f2.id) AS pdf_c,
-      #       count(DISTINCT f2.user_id) AS pdf_u,
-      #       count(DISTINCT f2.product_id) AS pdf_p
-      #     FROM
-      #       favorites f2
-      #     LEFT JOIN products p2 ON
-      #       p2.id = f2.product_id
-      #     WHERE
-      #       p2.soft_destroyed_at IS NULL
-      #       AND f2.amount IS NOT NULL
-      #     GROUP BY
-      #       p2.open_id
-      #   ) tb2 ON
-      #   tb2.open_id = o.id
-      # LEFT JOIN (
-      #     SELECT
-      #       p2.open_id,
-      #       count(f2.id) AS delete_c,
-      #       count(DISTINCT f2.user_id) AS delete_u
-      #     FROM
-      #       favorites f2
-      #     LEFT JOIN products p2 ON
-      #       p2.id = f2.product_id
-      #     WHERE
-      #       p2.soft_destroyed_at IS NULL
-      #       AND f2.soft_destroyed_at IS NOT NULL
-      #     GROUP BY
-      #       p2.open_id
-      #   ) tb3 ON
-      #   tb3.open_id = o.id
-      # LEFT JOIN (
-      #     SELECT
-      #       p2.open_id,
-      #       count(f2.id) AS pdf_delete_c,
-      #       count(DISTINCT f2.user_id) AS pdf_delete_u
-      #     FROM
-      #       favorites f2
-      #     LEFT JOIN products p2 ON
-      #       p2.id = f2.product_id
-      #     WHERE
-      #       p2.soft_destroyed_at IS NULL
-      #       AND f2.amount IS NOT NULL
-      #       AND f2.soft_destroyed_at IS NOT NULL
-      #     GROUP BY
-      #       p2.open_id
-      #   ) tb4 ON
-      #   tb4.open_id = o.id
-      # LEFT JOIN (
-      #     SELECT
-      #       p2.open_id,
-      #       count(f2.id) AS pdf_miss_c,
-      #       count(DISTINCT f2.user_id) AS pdf_miss_u
-      #     FROM
-      #       favorites f2
-      #     LEFT JOIN products p2 ON
-      #       p2.id = f2.product_id
-      #     WHERE
-      #       p2.soft_destroyed_at IS NULL
-      #       AND f2.soft_destroyed_at IS NOT NULL
-      #       AND f2.soft_destroyed_at < f2.created_at + INTERVAL '1hour'
-      #     GROUP BY
-      #       p2.open_id
-      #   ) tb5 ON
-      #   tb5.open_id = o.id
-      # LEFT JOIN (
-      #     SELECT
-      #       o2.id,
-      #       count(u.id) AS user_c
-      #     FROM
-      #       opens o2
-      #     LEFT JOIN users u ON
-      #       u.created_at < o2.bid_end_at
-      #     WHERE
-      #       u.confirmed_at IS NOT NULL
-      #     GROUP BY
-      #       o2.id
-      #   )
-      #   tb6 ON
-      #   tb6.id = o.id
-      # LEFT JOIN (
-      #     SELECT
-      #       p2.open_id,
-      #       count(dl.id) AS detail_c,
-      #       count(DISTINCT dl.utag) AS detail_utag,
-      #       count(DISTINCT dl.user_id) AS detail_u,
-      #       count(DISTINCT dl.product_id) AS detail_p
-      #     FROM
-      #       detail_logs dl
-      #     LEFT JOIN products p2 ON
-      #       p2.id = dl.product_id
-      #     WHERE
-      #       p2.soft_destroyed_at IS NULL
-      #     GROUP BY
-      #       p2.open_id
-      #   ) tb7 ON
-      #   tb7.open_id = o.id
-      # LEFT JOIN (
-      #     SELECT
-      #       p2.open_id,
-      #       count(p2.id) AS pc
-      #     FROM
-      #       products p2
-      #     WHERE
-      #       p2.soft_destroyed_at IS NULL
-      #     GROUP BY
-      #       p2.open_id
-      #   ) tb8 ON
-      #   tb8.open_id = o.id
-      # WHERE
-      #   o.id > 61
-      # ORDER BY
-      #   o.id
-      #     }
     when :feature_products
       %q{
 SELECT
